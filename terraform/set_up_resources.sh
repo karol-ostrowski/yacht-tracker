a6f4f9c3e1c711ff8e3126fa5f3d9fa6073882f2
Karol Ostrowski <karolostrowski111@gmail.com>
2025-10-07 22:33:26 +0200

    set up blob storage for monitoring

--- terraform/set_up_resources.sh
+++ terraform/set_up_resources.sh
@@ -1,3 +1,12 @@
 terraform apply -auto-approve
-echo "AZURE_CONNECTION_STRING=$(terraform output -raw connection_string)" > .env
-echo "AZURE_CONTAINER_NAME=$(terraform output -raw container_name)" >> .env
+
+echo "AZURE_CONNECTION_STRING=$(terraform output raw_data_connection_string)" > .env_raw_data
+echo "AZURE_CONTAINER_NAME=$(terraform output raw_data_container_name)" >> .env_raw_data
+
+echo "AZURE_STORAGE_ACCOUNT_NAME=$(terraform output monitoring_storage_account_name)" > .env_loki
+echo "AZURE_STORAGE_ACCOUNT_KEY=$(terraform output monitoring_storage_account_key)" >> .env_loki
+echo "AZURE_CONTAINER_NAME=$(terraform output loki_container_name)" >> .env_loki
+
+echo "AZURE_STORAGE_ACCOUNT_NAME=$(terraform output monitoring_storage_account_name)" > .env_mimir
+echo "AZURE_STORAGE_ACCOUNT_KEY=$(terraform output monitoring_storage_account_key)" >> .env_mimir
+echo "AZURE_CONTAINER_NAME=$(terraform output mimir_container_name)" >> .env_mimir
