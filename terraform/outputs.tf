18b471ba028caea1d58fcb7f60151743c2f22e2d
Karol Ostrowski <karolostrowski111@gmail.com>
2025-10-07 22:33:26 +0200

    set up blob storage for monitoring

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,26 @@
-output "connection_string" {
+output "raw_data_connection_string" {
   value     = azurerm_storage_account.rawdata.primary_connection_string
   sensitive = true
 }
 
-output "container_name" {
+output "raw_data_container_name" {
   value = azurerm_storage_container.rawdata.name
+}
+
+output "loki_container_name" {
+  value = azurerm_storage_container.loki.name
+
+}
+
+output "monitoring_storage_account_name" {
+  value = azurerm_storage_account.monitoring.name
+}
+
+output "monitoring_storage_account_key" {
+  value     = azurerm_storage_account.monitoring.primary_access_key
+  sensitive = true
+}
+
+output "mimir_container_name" {
+  value = azurerm_storage_container.mimir.name
 }
