9168cb01c3a18add87535572ea54d6d68d8ca9f0
Karol Ostrowski <karolostrowski111@gmail.com>
2025-10-13 10:06:04 +0200

    improved launch.sh

--- launch.sh
+++ launch.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
-echo "Provisioning cloud storage..."
-cd terraform
-./set_up_resources.sh
-cd ..
+#!/bin/bas
 
 echo "Setting up infrastructure..."
 cd infra
 docker compose up -d
 cd ..
 
-sleep 10
+echo "Provisioning cloud storage..."
+cd terraform
+./set_up_resources.sh
+cd ..
 
 echo "Setting up monitoring..."
 cd monitoring
